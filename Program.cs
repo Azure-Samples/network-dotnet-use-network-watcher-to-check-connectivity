ffc4966e70693e2e2e22efb880c81f1f83348cab
Chengming <v-cruan@microsoft.com>
2023-09-25 17:52:07 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -10,13 +10,8 @@ using Azure.ResourceManager.Resources;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Network;
 using Azure.ResourceManager.Network.Models;
-using Azure.ResourceManager.Storage.Models;
-using Azure.ResourceManager.Storage;
-using Microsoft.Identity.Client.Extensions.Msal;
 using Azure.ResourceManager.Compute.Models;
 using Azure.ResourceManager.Compute;
-using System.Xml.Linq;
-
 
 namespace VerifyNetworkPeeringWithNetworkWatcher
 {
@@ -51,20 +46,18 @@ namespace VerifyNetworkPeeringWithNetworkWatcher
 
         public static async Task RunSample(ArmClient client)
         {
-            string resourceGroupName =  SdkContext.RandomResourceName("rg", 15);
-            string vnetAName = SdkContext.RandomResourceName("net", 15);
-            string vnetBName = SdkContext.RandomResourceName("net", 15);
-
-            string[] vmNames = SdkContext.RandomResourceNames("vm", 15, 2);
+            string rgName = Utilities.CreateRandomName("NetworkSampleRG");
+            string vnetAName = Utilities.CreateRandomName("netA-");
+            string vnetBName = Utilities.CreateRandomName("netB-");
+            string vmName1 = Utilities.CreateRandomName("vm1-");
+            string vmName2 = Utilities.CreateRandomName("vm2-");
+            string networkWatcherName = Utilities.CreateRandomName("netwch");
+            string peeringABName = Utilities.CreateRandomName("peer");
             string[] vmIPAddresses = new String[] {
                 /* within subnetA */ "10.0.0.8",
                 /* within subnetB */ "10.1.0.8"
             };
 
-            string peeringABName = SdkContext.RandomResourceName("peer", 15);
-            string rootname = Utilities.CreateUsername();
-            string password = SdkContext.RandomResourceName("pWd!", 15);
-            string networkWatcherName = SdkContext.RandomResourceName("netwch", 20);
 
             {
                 // Get default subscription
@@ -80,59 +73,69 @@ namespace VerifyNetworkPeeringWithNetworkWatcher
                 //=============================================================
                 // Define two virtual networks to peer and put the virtual machines in, at specific IP addresses
 
-                List<ICreatable<INetwork>> networkDefinitions = new List<ICreatable<INetwork>>();
-                networkDefinitions.Add(azure.Networks.Define(vnetAName)
-                        .WithRegion(region)
-                        .WithNewResourceGroup(resourceGroupName)
-                        .WithAddressSpace("10.0.0.0/27")
-                        .WithSubnet("subnetA", "10.0.0.0/27"));
+                Utilities.Log("Creating a new virtual network...");
+                VirtualNetworkData vnetInput1 = new VirtualNetworkData()
+                {
+                    Location = resourceGroup.Data.Location,
+                    AddressPrefixes = { "10.0.0.0/27" },
+                    Subnets =
+                    {
+                        new SubnetData() { AddressPrefix = "10.0.0.0/27", Name = "subnetA" },
+                    },
+                };
+                var vnetLro1 = await resourceGroup.GetVirtualNetworks().CreateOrUpdateAsync(WaitUntil.Completed, vnetAName, vnetInput1);
+                VirtualNetworkResource vnet1 = vnetLro1.Value;
+                Utilities.Log($"Created a virtual network: {vnet1.Data.Name}");
 
-                networkDefinitions.Add(azure.Networks.Define(vnetBName)
-                        .WithRegion(region)
-                        .WithNewResourceGroup(resourceGroupName)
-                        .WithAddressSpace("10.1.0.0/27")
-                        .WithSubnet("subnetB", "10.1.0.0/27"));
+                Utilities.Log("Creating a new virtual network...");
+                VirtualNetworkData vnetInput2 = new VirtualNetworkData()
+                {
+                    Location = resourceGroup.Data.Location,
+                    AddressPrefixes = { "10.1.0.0/27" },
+                    Subnets =
+                    {
+                        new SubnetData() { AddressPrefix = "10.1.0.0/27", Name = "subnetB" },
+                    },
+                };
+                var vnetLro2 = await resourceGroup.GetVirtualNetworks().CreateOrUpdateAsync(WaitUntil.Completed, vnetBName, vnetInput2);
+                VirtualNetworkResource vnet2 = vnetLro2.Value;
+                Utilities.Log($"Created a virtual network: {vnet2.Data.Name}");
 
                 //=============================================================
                 // Define a couple of Windows VMs and place them in each of the networks
 
-                List<ICreatable<IVirtualMachine>> vmDefinitions = new List<ICreatable<IVirtualMachine>>();
-
-                for (int i = 0; i < networkDefinitions.Count; i++)
+                // Definate vm extension input data
+                string extensionName = "AzureNetworkWatcherExtension";
+                var extensionInput = new VirtualMachineExtensionData(resourceGroup.Data.Location)
                 {
-                    vmDefinitions.Add(azure.VirtualMachines.Define(vmNames[i])
-                        .WithRegion(region)
-                        .WithExistingResourceGroup(resourceGroupName)
-                        .WithNewPrimaryNetwork(networkDefinitions[i])
-                        .WithPrimaryPrivateIPAddressStatic(vmIPAddresses[i])
-                        .WithoutPrimaryPublicIPAddress()
-                        .WithPopularWindowsImage(KnownWindowsVirtualMachineImage.WindowsServer2012R2Datacenter)
-                        .WithAdminUsername(rootname)
-                        .WithAdminPassword(password)
+                    Publisher = "Microsoft.Azure.NetworkWatcher",
+                    ExtensionType = "NetworkWatcherAgentWindows",
+                    TypeHandlerVersion = "1.4",
+                    AutoUpgradeMinorVersion = true,
+                };
 
-                        // Extension currently needed for network watcher support
-                        .DefineNewExtension("packetCapture")
-                            .WithPublisher("Microsoft.Azure.NetworkWatcher")
-                            .WithType("NetworkWatcherAgentWindows")
-                            .WithVersion("1.4")
-                            .Attach());
-                }
+                // Create vm1
+                Utilities.Log("Creating a new virtual machine...");
+                NetworkInterfaceResource nic1 = await Utilities.CreateNetworkInterface(resourceGroup, vnet1);
+                VirtualMachineData vmInput1 = Utilities.GetDefaultVMInputData(resourceGroup, vmName1);
+                vmInput1.NetworkProfile.NetworkInterfaces.Add(new VirtualMachineNetworkInterfaceReference() { Id = nic1.Id, Primary = true });
+                var vmLro1 = await resourceGroup.GetVirtualMachines().CreateOrUpdateAsync(WaitUntil.Completed, vmName1, vmInput1);
+                VirtualMachineResource vm1 = vmLro1.Value;
+                _ = await vm1.GetVirtualMachineExtensions().CreateOrUpdateAsync(WaitUntil.Completed, extensionName, extensionInput);
+                Utilities.Log($"Created vm: {vm1.Data.Name}");
 
-                // Create the VMs in parallel for better performance
-                Utilities.Log("Creating virtual machines and virtual networks...");
-                var createdVMs = azure.VirtualMachines.Create(vmDefinitions);
-                IVirtualMachine vmA = createdVMs.FirstOrDefault(vm => vm.Key == vmDefinitions[0].Key);
-                IVirtualMachine vmB = createdVMs.FirstOrDefault(vm => vm.Key == vmDefinitions[1].Key);
-                Utilities.Log("Created the virtual machines and networks.");
+                // Create vm2
+                Utilities.Log("Creating a new virtual machine...");
+                NetworkInterfaceResource nic2 = await Utilities.CreateNetworkInterface(resourceGroup, vnet2);
+                VirtualMachineData vmInput2 = Utilities.GetDefaultVMInputData(resourceGroup, vmName2);
+                vmInput2.NetworkProfile.NetworkInterfaces.Add(new VirtualMachineNetworkInterfaceReference() { Id = nic2.Id, Primary = true });
+                var vmLro2 = await resourceGroup.GetVirtualMachines().CreateOrUpdateAsync(WaitUntil.Completed, vmName2, vmInput2);
+                VirtualMachineResource vm2 = vmLro2.Value;
+                _ = await vm2.GetVirtualMachineExtensions().CreateOrUpdateAsync(WaitUntil.Completed, extensionName, extensionInput);
+                Utilities.Log($"Created vm: {vm2.Data.Name}");
 
                 //=============================================================
                 // Peer the two networks using default settings
-
-                INetwork networkA = vmA.GetPrimaryNetworkInterface().PrimaryIPConfiguration.GetNetwork();
-                INetwork networkB = vmB.GetPrimaryNetworkInterface().PrimaryIPConfiguration.GetNetwork();
-
-                Utilities.PrintVirtualNetwork(networkA);
-                Utilities.PrintVirtualNetwork(networkB);
 
                 Utilities.Log(
                         "Peering the networks using default settings...\n"
@@ -140,46 +143,70 @@ namespace VerifyNetworkPeeringWithNetworkWatcher
                         + "- Traffic forwarding disabled\n"
                         + "- Gateway use (transit) by the remote network disabled");
 
-                INetworkPeering peeringAB = networkA.Peerings.Define(peeringABName)
-                        .WithRemoteNetwork(networkB)
-                        .Create();
-
-                Utilities.PrintVirtualNetwork(networkA);
-                Utilities.PrintVirtualNetwork(networkB);
+                Utilities.Log("Creating peering between vnetA and vnetB...");
+                VirtualNetworkPeeringData peeringBaseInput = new VirtualNetworkPeeringData()
+                {
+                    AllowVirtualNetworkAccess = true,
+                    AllowForwardedTraffic = true,
+                    AllowGatewayTransit = false,
+                    UseRemoteGateways = false,
+                    RemoteVirtualNetworkId = vnet2.Id,
+                };
+                // Create a peering at vnet1
+                VirtualNetworkPeeringData peeringInput1 = peeringBaseInput;
+                peeringInput1.RemoteVirtualNetworkId = vnet2.Id;
+                _ = await vnet1.GetVirtualNetworkPeerings().CreateOrUpdateAsync(WaitUntil.Completed, peeringABName, peeringInput1);
+                // Create a peering at vnet2 to establish a synchronized connection.
+                VirtualNetworkPeeringData peeringInput2 = peeringBaseInput;
+                peeringInput1.RemoteVirtualNetworkId = vnet1.Id;
+                _ = await vnet2.GetVirtualNetworkPeerings().CreateOrUpdateAsync(WaitUntil.Completed, peeringABName, peeringInput1);
+                // Get peering
+                var peeringLro = await vnet1.GetVirtualNetworkPeerings().GetAsync(peeringABName);
+                VirtualNetworkPeeringResource peering = peeringLro.Value;
+                Utilities.Log($"Created peering: {peering.Data.Name}");
 
                 //=============================================================
                 // Check connectivity between the two VMs/networks using Network Watcher
-                INetworkWatcher networkWatcher = azure.NetworkWatchers.Define(networkWatcherName)
-                        .WithRegion(region)
-                        .WithExistingResourceGroup(resourceGroupName)
-                        .Create();
+                Utilities.Log($"Create a network watcher in {resourceGroup.Data.Location}...");
+                Utilities.Log("To note: one subscription only has a Network Watcher in the same region");
+                Utilities.Log($"         make sure there has not Network Watcher in {resourceGroup.Data.Location}");
+                NetworkWatcherData networkWatcherInput = new NetworkWatcherData()
+                {
+                    Location = resourceGroup.Data.Location,
+                };
+                //var networkWatcherLro = await resourceGroup.GetNetworkWatchers().CreateOrUpdateAsync(WaitUntil.Completed, networkWatcherName, networkWatcherInput);
+                //NetworkWatcherResource networkWatcher = networkWatcherLro.Value;
+                var watcherRG = await subscription.GetResourceGroups().GetAsync("NetworkWatcherRG");
+                var networkWatcherLro = await watcherRG.Value.GetNetworkWatchers().GetAsync("NetworkWatcher_eastus");
+                NetworkWatcherResource networkWatcher = networkWatcherLro.Value;
 
                 // Verify bi-directional connectivity between the VMs on port 22 (SSH enabled by default on Linux VMs)
-                IExecutable<IConnectivityCheck> connectivityAtoB = networkWatcher.CheckConnectivity()
-                    .ToDestinationAddress(vmIPAddresses[1])
-                    .ToDestinationPort(22)
-                    .FromSourceVirtualMachine(vmA);
-                Utilities.Log("Connectivity from A to B: " + connectivityAtoB.Execute().ConnectionStatus);
+                // Block: waiting issue https://github.com/Azure/azure-sdk-for-net/pull/38876 fix
+                ConnectivityContent contentA2B = new ConnectivityContent(
+                   new ConnectivitySource(vm1.Id),
+                   new ConnectivityDestination() { Port = 22, ResourceId = vm2.Id });
+                var connectivityA2BResult = await networkWatcher.CheckConnectivityAsync(WaitUntil.Completed, contentA2B);
+                Utilities.Log("Connectivity from A to B: " + connectivityA2BResult.Value.NetworkConnectionStatus);
 
-                IExecutable<IConnectivityCheck> connectivityBtoA = networkWatcher.CheckConnectivity()
-                    .ToDestinationAddress(vmIPAddresses[0])
-                    .ToDestinationPort(22)
-                    .FromSourceVirtualMachine(vmB);
-                Utilities.Log("Connectivity from B to A: " + connectivityBtoA.Execute().ConnectionStatus);
+                ConnectivityContent contentB2A = new ConnectivityContent(
+                  new ConnectivitySource(vm2.Id),
+                  new ConnectivityDestination() { Port = 22, ResourceId = vm1.Id });
+                var connectivityB2AResult = await networkWatcher.CheckConnectivityAsync(WaitUntil.Completed, contentB2A);
+                Utilities.Log("Connectivity from B to A: " + connectivityB2AResult.Value.NetworkConnectionStatus);
 
                 // Change the peering to allow access between A and B
                 Utilities.Log("Changing the peering to disable access between A and B...");
-                peeringAB.Update()
-                    .WithoutAccessFromEitherNetwork()
-                    .Apply();
-
-                Utilities.PrintVirtualNetwork(networkA);
-                Utilities.PrintVirtualNetwork(networkB);
+                VirtualNetworkPeeringData peeringUpdateInput = new VirtualNetworkPeeringData()
+                {
+                    AllowVirtualNetworkAccess = false,
+                };
+                var updatePeeringLro = await peering.UpdateAsync(WaitUntil.Completed, peeringUpdateInput);
+                VirtualNetworkPeeringResource updatedPeering = updatePeeringLro.Value;
 
                 // Verify connectivity no longer possible between A and B
                 Utilities.Log("Peering configuration changed.\nNow, A should be unreachable from B, and B should be unreachable from A...");
-                Utilities.Log("Connectivity from A to B: " + connectivityAtoB.Execute().ConnectionStatus);
-                Utilities.Log("Connectivity from B to A: " + connectivityBtoA.Execute().ConnectionStatus);
+                Utilities.Log("Connectivity from A to B: " + connectivityA2BResult.Value.NetworkConnectionStatus);
+                Utilities.Log("Connectivity from B to A: " + connectivityB2AResult.Value.NetworkConnectionStatus);
             }
             {
                 try
